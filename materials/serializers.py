a6e4ab8e86bfcce33e353057c755ec190876667f
nick_krlv <fieruo@gmail.com>
2024-02-21 18:04:14 +0300

    Задание 1

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,9 +3,14 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return Lesson.objects.filter(course_id=obj.id).count()
+
     class Meta:
         model = Course
-        fields = ('name', 'description')
+        fields = ('name', 'description', 'lessons_count')
 
 
 class LessonSerializer(serializers.ModelSerializer):
